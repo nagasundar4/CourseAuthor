f667066547426e36990fe1a5e0a1baebc956bd37
Nagarajan Sundaram <nagasundar@gmail.com>
2023-12-23 14:42:38 -0500

    Fix issue with Glyphicon and Add Input and Output fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title!: string;
+  post = {
+    title: 'Title',
+    isFavorite: true,
+  };
+  onFavoriteChanged() {
+    console.log('Favorite changed');
+  }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent /* implements OnInit */ {
+  @Input('isFavorite')
+  isSelected: boolean = false;
+
+  @Output() change = new EventEmitter();
+
   isFavorite: boolean | undefined;
 
-  constructor() {}
-  ngOnInit() {}
+  // constructor() {}
+  // ngOnInit() {}
   onClick() {
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
 }
